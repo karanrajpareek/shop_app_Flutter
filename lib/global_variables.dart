6f7e30b4824ccc10a1b46dfa2a21038a9bad9acc
karan raj pareek <karanrajpareek@gmail.com>
2024-07-29 01:24:34 +0530

    Initial commit

---
+++ lib/global_variables.dart
@@ -0,0 +1,44 @@
+final products = [
+  {
+    'id' : '0',
+    'title' : 'Men\'s Nike Shoes',
+    'price' : 44.52 ,
+    'Sizes' : [8,9,10,11],
+    'imageUrl': 'assets/images/shoes_1.png',
+    'company' : 'Nike'
+  },
+    {
+    'id' : '1',
+    'title' : 'Adidas Shoes',
+    'price' : 22.12,
+    'Sizes' : [9,10,12],
+    'imageUrl': 'assets/images/shoes_2.png',
+    'company' : 'Adidas'
+  },
+    {
+    'id' : '2',
+    'title' : 'Bata women\'s Shoes',
+    'price' : 28.95,
+    'Sizes' : [8,9,10],
+    'imageUrl': 'assets/images/shoes_3.png',
+    'company' : 'Bata'
+  },
+  {
+    'id' : '3',
+    'title' : 'Jordan Shoes',
+    'price' : 420.69,
+    'Sizes' : [8,9,10],
+    'imageUrl': 'assets/images/shoes_4.png',
+    'company' : 'Nike'
+  }
+];
+final cart = [
+  {
+    'id' : '3',
+    'title' : 'Jordan Shoes',
+    'price' : 420.69,
+    'Size' : 8,
+    'imageUrl': 'assets/images/shoes_4.png',
+    'company' : 'Nike'
+}
+];
